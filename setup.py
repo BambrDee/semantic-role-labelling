7eacff2c19f11606466594813b1910c06a9d529b
BambrDee <BambrDee@gmail.com>
2022-12-26 07:36:40 +0300

    Fix setup.py

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(name='semantic_tagger',
       author='',
       author_email='',
       url='',
-      packages=['semantic_tagger', 'semantic_tagger.data', 'classifier.evaluation'],
+      packages=['semantic_tagger', 'semantic_tagger.data', 'semantic_tagger.evaluation'],
    	  package_data={'semantic_tagger.data': ['./*']})
